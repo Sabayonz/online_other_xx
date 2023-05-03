f604032b13fcb2be0837cb9575923e3bbba47ec3
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-05-03 13:07:49 +0700

    Update gw2x.py

--- gw2x.py
+++ gw2x.py
@@ -1,12 +1,12 @@
-mountstamina_base_mem = 0x02727F90
+mountstamina_base_mem = 0x0272D4F0
 mountstamina_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0xC]
 
-mounttype_base_mem = 0x02727F90
+mounttype_base_mem = 0x0272D4F0
 mounttype_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0x14]
 
 
-skyscalegreenbaraddress = 0x1089CCE
+skyscalegreenbaraddress = 0x108C47E
 
-griffoninstaboostaddress = 0x11CF41B
+griffoninstaboostaddress = 0x11D2F0B
 
 versionnow = "1.0.0.1"
