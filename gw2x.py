2ee1afd8cb275d1301cb6883ceb661984487a55d
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-06-07 03:30:09 +0700

    Update gw2x.py

--- gw2x.py
+++ gw2x.py
@@ -1,32 +1,33 @@
-mountstamina_base_mem = 0x027384F0
+mountstamina_base_mem = 0x02737D70
 mountstamina_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0xC]
 
-mounttype_base_mem = 0x027384F0
+mounttype_base_mem = 0x02737D70
 mounttype_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0x14]
 
 
-skyscalegreenbaraddress = 0x108DFFE
+skyscalegreenbaraddress = 0x10915BE
 
-griffoninstaboostaddress = 0x11D5D2B
+griffoninstaboostaddress = 0x11DABCB
+
 
 #==============
-xpos_base_mem = 0x02737C88
+xpos_base_mem = 0x02737D70
 xpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x120]
 
-zpos_base_mem = 0x02737C88
+zpos_base_mem = 0x02737D70
 zpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x124]
 
-ypos_base_mem = 0x02737C88
+ypos_base_mem = 0x02737D70
 ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
 #=============
 
-map_addr = 0x279451C
+map_addr = 0x27964FC
 
 
 
 
 versionnow = "1.0.0.1"
 
-mountmasterupdate = "Last Update : May 24, 2023"
+mountmasterupdate = "Last Update : June 7, 2023"
 
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nwell,for now, please enjoy the game in normal mode"
