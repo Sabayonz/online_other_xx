4b885e16d2b80a453942903a3d0953341a0fd479
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-07 04:44:02 +0700

    Update gw2x_pro.py

--- gw2x_pro.py
+++ gw2x_pro.py
@@ -4,7 +4,7 @@ skyscalegreenbaraddress = 0x109FA4E
 
 griffoninstaboostaddress = 0x11EB58B
 
-map_addr = 0x272AD0C
+map_addr = 0x0000000
 
 
 
@@ -33,7 +33,7 @@ ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
 
 versionnow = "1.0.0.1"
 
-mountmasterupdate = "Last Update : August 2, 2023 \n CLICK HERE FOR LEGEND LIST!"
+mountmasterupdate = "THIS VERSION IS OBSOLOTE \n NEW VERSION AVAILABE !!"
 
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
 
