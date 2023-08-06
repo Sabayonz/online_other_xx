fadf20a5c0d1ef40098284401cdb8f889639b053
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-07 04:45:23 +0700

    Update gw2x_pro.py

--- gw2x_pro.py
+++ gw2x_pro.py
@@ -33,7 +33,7 @@ ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
 
 versionnow = "1.0.0.1"
 
-mountmasterupdate = "THIS VERSION IS OBSOLOTE \n NEW VERSION AVAILABE !!"
+mountmasterupdate = "THIS VERSION IS OBSOLETE \n NEW VERSION AVAILABE !!"
 
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
 
