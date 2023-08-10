a0c96109a12cd5507eb56623c28865e80a9f3fc1
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-10 17:23:09 +0700

    Update GW2X_FULL.py

--- GW2X_FULL.py
+++ GW2X_FULL.py
@@ -39,11 +39,11 @@ ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
 #=============
 
 
-versionnow = "1.0.0.1"
+versionnow = "1.0.0.0"
 
 mountmasterupdate = "NEW VERSION AVAILABE ! \n download on Discord"
 
-messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
+messagex = "Err:NEW VERSION AVAILABLE.\nplease update the trainer"
 
 donator = """
 ==||== \n 
