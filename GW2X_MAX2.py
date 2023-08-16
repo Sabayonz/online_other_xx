7441cf2081ec10c602917e40c34dd7dd4bbe0d1a
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-16 10:47:51 +0700

    Create GW2X_MAX2.py

---
+++ GW2X_MAX2.py
@@ -0,0 +1,90 @@
+base_core_address = 0x026CC0E0
+
+skyscalegreenbaraddress = 0x109FA4E
+
+griffoninstaboostaddress = 0x11EB58B
+
+fishingmasteraddress = 0x10A5818
+
+instafishingaddress = 0x022AE2A0
+
+map_addr = 0x272AD0C
+
+
+
+mountstamina_base_mem = base_core_address
+mountstamina_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0xC]
+
+mounttype_base_mem = base_core_address
+mounttype_offset_mem = [0x38, 0x2D0, 0x18, 0x0, 0x60, 0x14]
+
+
+
+#==============
+fishing_base_mem = instafishingaddress
+fishing_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x80]
+
+grafity_base_mem = base_core_address
+grafity_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x294]
+
+speed_base_mem = base_core_address
+speed_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2B8]
+
+downrate_glider_base_mem = base_core_address
+downrate_glider_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2DC]
+
+
+ground_base_mem = base_core_address
+ground_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x118]
+
+xpos_base_mem = base_core_address
+xpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x120]
+
+zpos_base_mem = base_core_address
+zpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x124]
+
+ypos_base_mem = base_core_address
+ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
+#=============
+
+
+versionnow = "1.0.0.1"
+
+mountmasterupdate = "Last Update : August 10, 2023"
+
+mountmasterupdatex = "Last Update : August 12, 2023"
+
+messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P \n
+\n\n
+ASCENDED COMMANDER : \n
+- R Z , - N M S, - N H, -M D B, -J S\n
+\n\n\n\n
+============================\n
+name displayed is nickname choosen by the donator\n 
+consider donating to keep this project alive\n
+thanks for your support !
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","bc9c7d9eb2","2fda4ec1b0","d72f0e4f08"]
+
+'''
+
+b275bc6551 = don't mind me
+d05112dc9e = morbiddarkblood
+cf80d1275b = NtajHmoob
+f4437fc8fe = nemesis
+3678bde5e3 = intel_power
+bc9c7d9eb2 = sabay
+2fda4ec1b0 = kowa
+d72f0e4f08 = rietzee
+'''
+#=============
+#==============
