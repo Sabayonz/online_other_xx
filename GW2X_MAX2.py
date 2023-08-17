6ba294b493415e98339fc06554e783dc3ce8793d
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-17 19:11:31 +0700

    Update GW2X_MAX2.py

--- GW2X_MAX2.py
+++ GW2X_MAX2.py
@@ -68,7 +68,7 @@ ASCENDED COMMANDER : \n
 ============================\n
 name displayed is nickname choosen by the donator\n 
 consider donating to keep this project alive\n
-thanks for your support !
+thanks for your support !!
 """
 #=============
 #=============
