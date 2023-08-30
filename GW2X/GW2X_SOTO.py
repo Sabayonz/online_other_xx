7cbefed0cf4cc5152b354f560e5200e4e4d96820
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-08-30 21:38:53 +0700

    Update GW2X_SOTO.py

--- GW2X/GW2X_SOTO.py
+++ GW2X/GW2X_SOTO.py
@@ -83,7 +83,7 @@ LEGENDARY COMMANDER : \n
 \n\n
 ASCENDED COMMANDER : \n
 - R Z , - N M S, - N H, -M D B, -J S, -J A, - F C
-\n -V R, -M D G, -R H, -P L C, -F O X, -S N, -O Y I, -W G
+\n -V R, -M D G, -R H, -P L C, -F O X, -S N, -O Y I, -W G, - E S
 \n\n\n\n
 ============================\n
 that name displayed is the donator\n
@@ -95,9 +95,10 @@ thanks for your support !!!
 #=============
 #=============
 
-uuid_saved = ["d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","2fda4ec1b0","d72f0e4f08"]
+uuid_saved = ["33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","2fda4ec1b0","d72f0e4f08"]
 
 '''
+33bfcbbadc = EddieSmurphy
 d35d2ec041 = wongo
 2bd4280a7d = Truckingsteve#007
 df33e92ff7 =  OD YR INK
