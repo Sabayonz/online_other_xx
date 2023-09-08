0eccf79da80b3d84d6589c4a26a644cb282d49ee
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-09-08 19:05:31 +0700

    Update GW2X_SOTO2.py

--- GW2X/GW2X_SOTO2.py
+++ GW2X/GW2X_SOTO2.py
@@ -76,7 +76,7 @@ versionnow = "1.0.0.1"
 
 
 
-mountmasterupdatex = "Last Update : September 03, 2023"
+mountmasterupdatex = "Last Update : September 08, 2023"
 
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
 
