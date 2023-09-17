1f7f9623c4a5540d2dfc9c5a3e99a32a36d28737
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-09-17 22:17:57 +0700

    Update GW2X_SOTO2.py

--- GW2X/GW2X_SOTO2.py
+++ GW2X/GW2X_SOTO2.py
@@ -101,7 +101,7 @@ thanks for your support !!!
 #=============
 #=============
 
-uuid_saved = ["96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","43486e68b8","e53d258881","75a4159f86","c155f9c7d4","01035bf5b7","68085ef0f0","8a3ebb6c8b","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0"]
+uuid_saved = ["96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","e53d258881","75a4159f86","c155f9c7d4","01035bf5b7","68085ef0f0","8a3ebb6c8b","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0","51731556be"]
 
 '''
 
@@ -150,7 +150,7 @@ bc9c7d9eb2 = sabay
 d1ee7c0888 = Robbes
 d72f0e4f08 = rietzee
 
-43486e68b8 = VM 1
+51731556be = VM 1
 '''
 #=============
 #==============
