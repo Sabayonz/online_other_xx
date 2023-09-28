f63d17018cdee539ad91954157382ad8e28a700c
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-09-28 12:35:08 +0700

    Create GW2X_SOTO3.py

---
+++ GW2X/GW2X_SOTO3.py
@@ -0,0 +1,177 @@
+base_core_address = 0x026B8970
+
+skyscalegreenbaraddress = 0x10AE8BE
+
+griffoninstaboostaddress = 0x11F1ECB
+
+fishingmasteraddress = 0x10B3FBA
+
+instafishingaddress = 0x0229C8D0
+
+clippingaddress = 0x13044B9
+
+fullclippingaddress = 0x13044BD
+
+map_addr = 0x27176FC
+
+interacting = 0x276435C
+interacting_value = 32000
+
+#==============
+
+mountstamina_base_mem = base_core_address
+mountstamina_offset_mem = [0x38, 0x2D8, 0x18, 0x0, 0x60, 0xC]
+
+mounttype_base_mem = base_core_address
+mounttype_offset_mem = [0x38, 0x2D8, 0x18, 0x0, 0x60, 0x14]
+
+fishing_insta_hook_base_mem = instafishingaddress
+fishing_insta_hook_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x68]
+
+fishing_insta_signing_base_mem = instafishingaddress
+fishing_insta_signing_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x5F]
+
+
+fishing_base_mem = instafishingaddress
+fishing_offset_mem = [0x0, 0x0, 0x60, 0x98, 0x0, 0x20, 0x80]
+
+grafity_base_mem = base_core_address
+grafity_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x294]
+
+wallclimb_base_mem = base_core_address
+wallclimb_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x29C]
+
+speed_base_mem = base_core_address
+speed_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2B8]
+
+speed_base_mem1 = base_core_address
+speed_offset_mem1 = [0x38, 0x28, 0x80, 0x38, 0x2B4]
+
+speed_base_mem2 = base_core_address
+speed_offset_mem2 = [0x38, 0x28, 0x80, 0x38, 0x2B0]
+
+downrate_glider_base_mem = base_core_address
+downrate_glider_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x2DC]
+
+vypos_base_mem = base_core_address
+vypos_offset_mem = [0x38, 0x28, 0x38, 0x80, 0x38, 0x1A8]
+
+
+ground_base_mem = base_core_address
+ground_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x118]
+
+xpos_base_mem = base_core_address
+xpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x120]
+
+zpos_base_mem = base_core_address
+zpos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x124]
+
+ypos_base_mem = base_core_address
+ypos_offset_mem = [0x38, 0x28, 0x80, 0x38, 0x88, 0x78, 0x128]
+#=============
+LWS4_EOD_AND_SOTO_MAP_LIST = [1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452]
+pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
+
+versionnow = "1.0.0.1"
+
+
+
+mountmasterupdatex = "UPDATE AVAILABLE \n FIX AUTO LOOT/INTERACT/VISTA VIEW\n DOWNLOAD ON ELITEPVP OR DISCORD"
+
+mountmasterupdate = "Last Update : September 27, 2023"
+
+messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, \n
+\n\n
+ASCENDED COMMANDER : \n
+- R Z , - N M S, - N H, -M D B, -J S, -J A, - F C, -R B
+ -V R, -M D G, -R H, -P L C, -F O X, -S N, -O Y I, -W G, - E S
+ -M Q, -R D, -B K, N K, -T T, -A C, - R P,  -I V, -J X,
+-R S, - A K M, - F R T Z, -S P, -V L, -F Q, - L X, - D Z, -E R, -C J, -L C\n
+-N J, - M Z, -T H, -A K, -T D, -L M, -N E, -R D N, -B C, - J M, - C C\n
+============================\n
+that name displayed is the donator\n
+if you donator and want specific nickname, just let me know\n
+consider donating to keep this project alive\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["0f807d59e8","cad32eb6ab","0f15f4702e","613007de10","c3d30ec84c","3afeaf99ff","9756b3c68e","a57c519973","199879ef8d","1ff58644c8","b7c0fe27f5","2bdc737541","06dc312134","93582e4eae","afee16fe47","ecc010a66e","d461de7177","96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","e53d258881","75a4159f86","adb1df7adf","01035bf5b7","68085ef0f0","8251db86c5","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","2ffeb1091c","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0","51731556be"]
+
+'''
+
+0f807d59e8 = Cekcenk
+cad32eb6ab =  jiimmy
+0f15f4702e = Biscotti
+613007de10 = RdN
+3afeaf99ff = No62NoExit
+9756b3c68e = Lucas--MileFantazy
+a57c519973 = Taldor
+199879ef8d = akull123456
+1ff58644c8 = TheHacker
+b7c0fe27f5 = Mizunie
+06dc312134 = NJ
+93582e4eae = khicon
+d461de7177 = Level 1 Crook
+96029128c9 = carrjam
+f251ad584e = Error
+6b6c5a4f88 = Dumnezeus
+b679c63915 = Luxz
+9ce4111361 = Freq
+e53d258881 = Villain
+75a4159f86 = SquishyPoo1870
+c155f9c7d4 = FRTZ
+adb1df7adf = FRTZ new Vm
+01035bf5b7 = Akemi
+8a3ebb6c8b = Robsie Remove
+8251db86c5 = Robsie New
+f106ab283d = J4XXXOR
+f89f95cbcc = ivicask
+c3d30ec84c = ivicask 2
+da698b0e4c = RarePepe
+d7e53d75f2 = Aiden Cross 
+3496ec8c63 = tiredtbh
+e2823149be = Naksu
+01035bf5b7 = Beutelkramer
+e84e400c61 = ridicul0us
+2bdc737541 = Morqitta 3
+68085ef0f0 = Morqitta 2
+9e87fbea81 = Morqitta new
+33bfcbbadc = EddieSmurphy
+d35d2ec041 = wongo
+2bd4280a7d = Truckingsteve#007
+df33e92ff7 =  OD YR INK
+5eb7799781 = Syrix == not confirmed, i add x on the id
+090afbe976 = Sinic
+7e10425e1b = フォックスマン
+2ffeb1091c = polychromatik
+d6fbdc1520 = Reeeehaww
+0f407ed34e = Mindaugas
+5debc04d27 = Hawaiian
+da2569d097 = Varek
+30fac06e50 = falco
+6fe74d85f3 = Jaxx Armstrong
+ecc010a66e = don't mind me 3
+b275bc6551 = don't mind me 2
+53b625f3d9 = don't mind me
+d05112dc9e = morbiddarkblood
+cf80d1275b = NtajHmoob
+f4437fc8fe = nemesis
+3678bde5e3 = intel_power
+bc9c7d9eb2 = sabay
+2fda4ec1b0 = kowa ( GIVE ACCOUNT TO Robbes )
+d1ee7c0888 = Robbes
+d72f0e4f08 = rietzee
+
+51731556be = VM 1
+afee16fe47 = vm 2
+'''
+#=============
+#==============
