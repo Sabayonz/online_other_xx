f3ebb0b5f4f9b6e5a43c0c569ade403583b0bc0b
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-10-14 08:21:49 +0700

    Update GW2X_SOTO3.py

--- GW2X/GW2X_SOTO3.py
+++ GW2X/GW2X_SOTO3.py
@@ -95,7 +95,7 @@ ASCENDED COMMANDER : \n
 RZ, NMS, NH, MDB, JS, JA, FC, RB, VR, MDG, RH, PLC, FOX, SN, OYI, 
 WG, ES, MQ, RD, BK, NK, TT, AC, RP, IV, JX, RS, AKM, FRT, SP, VL, 
 FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM, 
-CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR\n
+CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR\n
 ============================\n
 that name displayed is the donator\n
 if you donator and want specific nickname, just let me know\n
@@ -106,11 +106,11 @@ thanks for your support !!!
 #=============
 #=============
 
-uuid_saved = ["fec12d3dfe","45c31f67d6","54d6f759c8","27ad43a152","8bdb6dba2f","40dc55a18b","2402f332f3","4b49224938","05c45dae6a","602b7b391b","1af7f3316f","613007de10","c178ab4972","6a5db5599a","fee0c0b244","97c795658d","cad32eb6ab","0f15f4702e","613007de10","c3d30ec84c","3afeaf99ff","9756b3c68e","a57c519973","199879ef8d","1ff58644c8","b7c0fe27f5","2bdc737541","06dc312134","93582e4eae","afee16fe47","ecc010a66e","d461de7177","96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","e53d258881","75a4159f86","56baf74144","01035bf5b7","68085ef0f0","8251db86c5","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","c34b37dfd9","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0","51731556be"]
+uuid_saved = ["7fe8a30c36","fec12d3dfe","45c31f67d6","54d6f759c8","27ad43a152","8bdb6dba2f","40dc55a18b","2402f332f3","4b49224938","05c45dae6a","602b7b391b","1af7f3316f","613007de10","c178ab4972","6a5db5599a","fee0c0b244","97c795658d","cad32eb6ab","0f15f4702e","613007de10","c3d30ec84c","3afeaf99ff","9756b3c68e","a57c519973","199879ef8d","1ff58644c8","b7c0fe27f5","2bdc737541","06dc312134","93582e4eae","afee16fe47","ecc010a66e","d461de7177","96029128c9","f251ad584e","6b6c5a4f88","b679c63915","9ce4111361","e53d258881","75a4159f86","56baf74144","01035bf5b7","68085ef0f0","8251db86c5","f106ab283d","f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041","2bd4280a7d","53b625f3d9","df33e92ff7","x5eb7799781","090afbe976","7e10425e1b","c34b37dfd9","d6fbdc1520","0f407ed34e","5debc04d27","bc9c7d9eb2","da2569d097","30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0","51731556be"]
 
 '''
 
-
+7fe8a30c36 = Steryan
 fec12d3dfe = xtraRice
 45c31f67d6 = Bolt
 54d6f759c8 = Cekko
