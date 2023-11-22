18d471c7cc106bf5e9ad8191d1c48a4c30d5b86b
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-11-22 21:37:20 +0700

    Create GW2X_NOV_22.py

---
+++ GW2X/GW2X_NOV_22.py
@@ -0,0 +1,37 @@
+#49 FF 39 2E F6 7F 00 00 01
+base_core_address_ptr = [0x49, 0xFF, 0x39, 0x2E, 0xF6, 0x7F, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00]
+base_core_address_key = 160
+
+
+map_addr = 0x226F8B0
+
+interacting = 0x2788C2C
+interacting_key = 0
+
+mountzom = 0x0278AB00
+mountzom_key = 0
+
+playerzom = 0x026E7470
+playerzom_key = 0
+
+
+
+update_01 = "Last Update : November 22, 2023"
+
+
+
+
+
+
+
+
+
+
+
+
+LWS4_EOD_AND_SOTO_MAP_LIST = [1526, 1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452]
+pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
+
+
+versionnow = "1.0.0.1"
+messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
