0358b35597e0f3f79d08a97faa4f1e7bd10ac075
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-11-26 05:39:33 +0700

    Create GW2X_NOV_26.py

---
+++ GW2X/GW2X_NOV_26.py
@@ -0,0 +1,61 @@
+base_core_address = 0x02270040
+
+skyscalegreenbaraddress = 0x10CF2DE
+
+griffoninstaboostaddress = 0x1217A0B
+
+fishingmasteraddress = 0x10D4ECF
+
+skiffmasteraddress = 0x1217B9E
+
+clippingaddress = 0x132B269
+
+fullclippingaddress = 0x132B26D
+
+map_addr = 0x226F8B0
+
+interacting = 0x2788C2C
+interacting_value = 30000
+
+fgaddr = 0xED52B
+
+fg2addr = 0xED531
+
+jeneng = 0x603B29
+
+padang = 0x616D31
+
+viewdist = 0x613C77
+
+darah1 = 0x615E05
+
+darah2 = 0x61709D
+
+mprdr = 0x4D2F4F
+
+objjeneng = 0x617571
+
+objdarah = 0x617764
+
+mountzom = 0x0278AB00
+
+playerzom = 0x026E7470
+
+skiffx = 0x0224F858
+
+
+update_01 = "Last Update : November 25, 2023"
+
+
+
+
+
+
+
+
+LWS4_EOD_AND_SOTO_MAP_LIST = [1526, 1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452]
+pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
+
+
+versionnow = "1.0.0.1"
+messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
