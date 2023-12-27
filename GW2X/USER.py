fe04625e3980499952bc8e7779e49af2c061cf43
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2023-12-27 17:09:07 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -32,7 +32,7 @@ uuid_saved = ["9aeaaceef9","0d5192aaa8","2d3300c56b","7e01fb6c55","1bcd371a24","
               "f89f95cbcc","da698b0e4c","d7e53d75f2","3496ec8c63","e2823149be","01035bf5b7","e84e400c61","9e87fbea81","33bfcbbadc","d35d2ec041",
               "2bd4280a7d","53b625f3d9","3534d65702","090afbe976","7e10425e1b","c34b37dfd9","d6fbdc1520","31607ab4c7","5debc04d27","da2569d097",
               "30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0",
-              "665b24b4cb","2ba0d93345","2e1d27765c","718f04c74e","4320d52d28",
+              "665b24b4cb","2ba0d93345","2e1d27765c","718f04c74e","4320d52d28","b427b63072","8b07915f7e",
               "f25a6280ce"]
 
 
@@ -106,6 +106,8 @@ b7c0fe27f5 = Mizunie
 d461de7177 = Level 1 Crook
 42339b90f9 = Level 1 Crook 2
 26c741dda2 = Level 1 Crook 3
+b427b63072 = Level 1 Crook 4
+8b07915f7e = Level 1 Crook 5
 96029128c9 = carrjam
 f251ad584e = Error
 6b6c5a4f88 = Dumnezeus
