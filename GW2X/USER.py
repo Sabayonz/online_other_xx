ae7152408253c2a73f31a261a9e6d960c6ee1479
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-01-26 19:25:08 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -11,7 +11,7 @@ FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM,
 CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR, EZP,
 AZ, CB, DY, KM, ACT, PM, AB, SX, AB, RP, DN, NN, AS, AF, VM, TS, CT,
 KG, TX, DP, NEO, KL, MT, KN, DMR, TZ, HB, VC, MK, NN, YC, MC, IS,
-SS, XK, AK, AT, CW \n
+SS, XK, AK, AT, CW, SL, \n
 ============================\n
 
 consider donating to keep this project alive\n
