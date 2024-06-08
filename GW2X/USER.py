b9f51de2720b7c3d6a6f018d69121a9cd23d348d
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-06-08 20:28:54 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -33,7 +33,7 @@ uuid_saved = ["9aeaaceef9","0d5192aaa8","2d3300c56b","7e01fb6c55","1bcd371a24","
               "2bd4280a7d","53b625f3d9","3534d65702","090afbe976","7e10425e1b","c34b37dfd9","d6fbdc1520","31607ab4c7","5debc04d27","9bc555c3de",
               "30fac06e50","6fe74d85f3","b275bc6551","d05112dc9e","cf80d1275b","f4437fc8fe","3678bde5e3","d1ee7c0888","d72f0e4f08","2fda4ec1b0",
               "665b24b4cb","2ba0d93345","2e1d27765c","18976cdcf1","4320d52d28","b427b63072","8b07915f7e","e640683c72","3ece663699","f334e9a3ac",
-              "0a1e3b4a9c","93582e4eae","5a46130f53","eb020857aa","2d8c04efd3","78d33186bb","f5fd6e7ce7","d1821fb798","5d4adcbfaf","8dcc3f17ce",
+              "0a1e3b4a9c","93582e4eae","5a46130f53","eb020857aa","2d8c04efd3","78d33186bb","47a3a5b8dd","d1821fb798","5d4adcbfaf","8dcc3f17ce",
               "00b5d7c72a","91c01b6dbb","75f995c8e6","5d7ceb6425","596851a385","b8519516f4","386423dc44","64e0c9c9d9","c0d43a2075","0c28dc0eb4",
               "f7ee09dae4","3b35e1873e","bee9f13140","1f9fa1ada9","5ba1df24d5","498283913a","5fef652218",
               "f25a6280ce"]
@@ -64,7 +64,7 @@ f7ee09dae4 = systemsoldier
 8dcc3f17ce = Isso
 5d4adcbfaf = Mercy
 d1821fb798 = jasonyc YC
-f5fd6e7ce7 = Neorinakota
+47a3a5b8dd = Neorinakota
 78d33186bb = Mohki
 eb020857aa = Vincef
 5a46130f53 = hibrick
