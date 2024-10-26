a78927d2b16afea7f7026e1ddb5b80fd28642662
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-10-27 01:24:22 +0700

    Add files via upload

---
+++ GW2X/NEW_USER.py
@@ -0,0 +1,235 @@
+
+donator = """
+==||== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, D M M \n
+\n\n
+ASCENDED COMMANDER : \n
+RZ, NMS, NH, MDB, JS, JA, FC, RB, VR, MDG, RH, PLC, FOX, SN, OYI, 
+WG, ES, MQ, RD, BK, NK, TT, AC, RP, IV, JX, RS, AKM, FRT, SP, VL, 
+FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM, 
+CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR, EZP,
+AZ, CB, DY, KM, ACT, PM, AB, SX, AB, RP, DN, NN, AS, AF, VM, TS, CT,
+KG, TX, DP, NEO, KL, MT, KN, DMR, TZ, HB, VC, MK, NN, YC, MC, IS,
+SS, XK, AK, AT, CW, SL, DD, AS, AV, GH, KK, FK, GM, MM, MF, RG,
+RS, CV, XC, GR, OD, SK, SC, SJ, NC, HG, VK, TY, BB, DV, AU, GB, HC,
+UL, RZ, AP, DW, GC, HM, SV, CG, CP, RW, VC, UM, PC, XR, NT, MC,
+KK, HF, MF, JN, PP, HZ, KJ, RN, GV, CT, CM, HJ, \n
+============================\n
+
+GW2X Exist and maintained since April 2023\nconsider donating to keep this project alive\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["d0b969800d83"]
+
+
+
+
+'''
+d5d1d4a10d = hjjr78
+01035bf5b7 = clementming
+b3fb075ca2 = CraTzy
+cc929ca7aa = GNGR_VKNG
+63380e15ce = Runnae (saekoslab)
+c62de1e69f = Kinjake
+666a11bf6e = Hizzy
+3140d0b61d = Porpor
+bd911b6a34 = j4n
+57c44476d8 = Mheleficent
+58384f5f26 = Hamad771 friends
+7a5725a7b7 = Kookoochoo 8loise
+19e23f97e5 = mc35879
+9a8205abcd = nesstor1099
+9b1a7b63a3 = Xryeth khal4843
+40e3c97750 = Porchy
+3547585727 = universeelementor_63535
+a4b3afed94 = vector_4444 wise old man
+e99609f8e1 = reisw
+e19b4431b1 = christmas_pud
+1ede003201 = chargovinda
+602858b102 = Silviohc
+7074de0998 = hamad771
+04deef56af = gr4cko
+f90bb82505 = nelson (doowen)
+515aee78c8 = appledinner
+811060718e = razpdr
+a34ede050a = morganj23
+5ecd9b1caf = hoilch
+bb17567ea0 = gold3nb0i
+cd591db0ae = auburnsprings
+59c68d8998 = david
+2730fe07c6 = beanboifresh
+415cd4e21d = tyrantt0_36151
+8b681b9078 = akulf123 riend
+e53d258881 = vekqurin
+7c95a149d9 = hudsightgamer
+a935b57bf9 = nicocg
+93582e4eae = ssaljoone
+e0a8f8edaa = scatterfrog
+666a11bf6e = skullylol
+5b1ef394ad = oddphantomhayden
+bd9f2ddb16 = Guerini
+df0bb99b35 = xChasseur
+71bf36080d = Corvo (liarliarpunsonfyre)
+5af3d5acf8 = RedSkyMessenger
+5fef652218 = Adam AC
+d28f134389 = temporary trial (removed)
+ed142892e3 = rdenog
+5ba1df24d5 = MechFan ihateusingdisc
+0c28dc0eb4 = MMoaddict
+c0d43a2075 = Gumagan
+64e0c9c9d9 = Father Kofo
+386423dc44 = Kinnikuman
+b8519516f4 = Greenhaus
+596851a385 = Avadon
+75f995c8e6 = Assassin
+91c01b6dbb = daddio9646
+00b5d7c72a = shlomotion
+1f9fa1ada9 = ChillyWilly
+5d7ceb6425 = ChillyWilly2
+122cc8f90b = Alte
+bee9f13140 = Arke
+e700072615 = xk xkxina
+f7ee09dae4 = systemsoldier
+8dcc3f17ce = Isso
+5d4adcbfaf = Mercy
+d1821fb798 = jasonyc YC
+47a3a5b8dd = Neorinakota
+78d33186bb = Mohki
+eb020857aa = Vincef
+5a46130f53 = hibrick
+93582e4eae = tanztante
+0a1e3b4a9c = kanei
+f334e9a3ac = Dr Murphy Richard
+c06cb2a792 = Matt77 2 (World Explorer)
+e640683c72 = Matt77 (World Explorer)
+4320d52d28 = Kil
+18976cdcf1 = neo14a
+2d3300c56b = Doople
+2d8c04efd3 = Doople 2
+7e01fb6c55 = TUX1337
+8ccb5171bc = Knallgurke 2
+48e98a00fb = Knallgurke
+fc5966ec43 = Critias
+357dcf535a = TSAO
+0d0f19b7a1 = TSAO 2
+0c28dc0eb4 = Afflictedfury
+5044c0deb2 = Astro
+97f88c63d7 = Nossninja
+c009ac19cc = Don
+596851a385 = Røpke
+6c6f68f289 = Røpke off
+28ca2077b2 = ASTRO 80Y
+bfd65c5093 = Shaxen
+ccda0f3c76 = Articvb
+1433e3fbf0 = Articvb 2
+784d42e8b8 = Private Marx
+7da945130a = Alcatraz 
+bc399d7191 = Kilmanagh
+c155f9c7d4 = dokyun
+d2e264842a = colebear
+7457621e28 = AzRa
+3034d3fdad = EdZoPlay
+91bc6add19 = TheTwork Sveik584
+7fe8a30c36 = Steryan
+fec12d3dfe = xtraRice
+45c31f67d6 = Bolt
+1f484708ec = Cekko
+27ad43a152 = TheTwork
+8bdb6dba2f = RenjaHB
+40dc55a18b = FamousFarrow
+2402f332f3 = Morticus
+4b49224938 = Sam Silva
+05c45dae6a = Tiger
+1af7f3316f = sasquash
+613007de10 = Daunt
+3778c59307 = LordVader removed
+6a5db5599a = Dutchwise
+fee0c0b244 = HiGW
+0f807d59e8 = Cekcenk
+97c795658d = Cekcenk new vm 
+cad32eb6ab =  jiimmy
+0f15f4702e = Biscotti
+613007de10 = RdN
+c178ab4972 = No62NoExit 2 laptop
+f151b202dc = No62NoExit
+91549412a3 = No62NoExit 3 (off, waiting new pc)
+3afeaf99ff = No62NoExit 4 
+da8ec3ebfd = Lucas--MileFantazy
+8e3d14b258 = Lucas--MileFantazy 2
+e00dddf217 = Lucas--MileFantazy 3
+ca02c15aa2 = Taldor
+171ad776bc = akull123456
+1ff58644c8 = TheHacker old off
+a014c6f860 = TheHacker
+b7c0fe27f5 = Mizunie
+1bcd371a24 = Mizunie 2
+0d5192aaa8 = Mizunie 3
+9aeaaceef9 = Mizunie 4
+06dc312134 = NJ
+93582e4eae = khicon
+d461de7177 = Level 1 Crook
+42339b90f9 = Level 1 Crook 2
+26c741dda2 = Level 1 Crook 3
+b427b63072 = Level 1 Crook 4
+8b07915f7e = Level 1 Crook 5
+96029128c9 = carrjam
+f251ad584e = Error
+6b6c5a4f88 = Dumnezeus
+b679c63915 = Luxz
+9ce4111361 = Freq
+e53d258881 = Villain
+75a4159f86 = SquishyPoo1870
+10808ae13a = FRTZ
+c02d618675 = FRTZ 2
+01035bf5b7 = Akemi
+1680963910 = Robsie
+f106ab283d = J4XXXOR
+f89f95cbcc = ivicask
+c3d30ec84c = ivicask 2
+da698b0e4c = RarePepe
+d7e53d75f2 = Aiden Cross 
+3496ec8c63 = tiredtbh
+e2823149be = Naksu
+01035bf5b7 = Beutelkramer
+e84e400c61 = ridicul0us
+2bdc737541 = Morqitta 3
+68085ef0f0 = Morqitta 2
+9e87fbea81 = Morqitta new
+33bfcbbadc = EddieSmurphy
+d35d2ec041 = wongo
+2bd4280a7d = Truckingsteve#007
+df33e92ff7 =  OD YR INK off
+3534d65702 = Kaisen OD YR INK
+5eb7799781 = Syrix removed
+090afbe976 = Sinic
+7e10425e1b = フォックスマン
+2ffeb1091c = polychromatik off
+c34b37dfd9 = polychromatik new
+d6fbdc1520 = Reeeehaww
+0f407ed34e = Mindaugas OFF
+31607ab4c7 = Mindaugas New
+5debc04d27 = Hawaiian
+da2569d097 = Varek
+30fac06e50 = falco
+6fe74d85f3 = Jaxx Armstrong
+ecc010a66e = don't mind me 3
+b275bc6551 = don't mind me 2
+53b625f3d9 = don't mind me
+d05112dc9e = morbiddarkblood
+cf80d1275b = NtajHmoob
+f4437fc8fe = nemesis
+6753ae7c20 = Nemesiss 2
+3678bde5e3 = intel_power
+2fda4ec1b0 = kowa ( GIVE ACCOUNT TO Robbes )
+d1ee7c0888 = Robbes
+d72f0e4f08 = rietzee
+d0b969800d83 = sabay
+
+'''
+#=============
+#==============
