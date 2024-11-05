a200785fa3e2cabe882fad6cf345c5b5125069b1
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-11-06 06:19:34 +0700

    Update X_CORE.py

--- GW2X/X_CORE.py
+++ GW2X/X_CORE.py
@@ -188,5 +188,5 @@ zzzz32 = 30580
 core_update = "Core: Oct_11_2024"
 LWS4_EOD_AND_SOTO_MAP_LIST = [1226, 1248, 1526, 1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452, 1550, 1554]
 pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
-versionnow = "1.0.0.1"
+versionnow = "1.0.0.3"
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
