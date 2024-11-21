c2e47bf25ecdb41bd1ca3349e5be76dc74b0b0bf
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-11-21 14:29:53 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -29,7 +29,7 @@ uuid_saved = ["fc81b935647d","260d27c38695","c5d83ec5fc2b","defaaeb70270","bbe4d
              "0d1a1868620a","45e6a9b90206","28225c679d0e","dbfae3746772","4879801bc9b0","70350bfd0594","41a6b478d099","e47dd58e0bfe","21a5de4cdc05","3e5fe19c3927",
              "f36d794590f5","cfa2b26cd8ca","bcb99ccdd20c","9e36f86b0008","9ea42576fdaa","14dd987aade2","8e07e13880d9","a7d217159b3e","8651b2fea7b2","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","80f6025a1b65","0fe30d9aefb7","e73f19de755e","641de1a4ffef","a9dccb506c93","0c7b7b294bfd","fc71b4ce26ac","2230199d200d",
-             "cdca8c0c222d","a34b330c24ae","c20af37ca609","0aa927848ddd","3fcce47d37b1","26b06274a393","c1db8b85552b","19235935724e"]
+             "cdca8c0c222d","a34b330c24ae","c20af37ca609","0aa927848ddd","3fcce47d37b1","26b06274a393","c1db8b85552b","19235935724e","39d6d903b3e1"]
 
 
 
@@ -222,7 +222,7 @@ df33e92ff7 =  OD YR INK off
 fc71b4ce26ac = Kaisen OD YR INK (hay_low)
 5eb7799781 = Syrix removed
 090afbe976 = Sinic
-7e10425e1b = フォックスマン
+39d6d903b3e1 = フォックスマン shermilion
 2ffeb1091c = polychromatik off
 c34b37dfd9 = polychromatik new
 d6fbdc1520 = Reeeehaww
