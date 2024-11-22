812db36c382a85cb9783049d39b180025d2fa53d
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-11-22 18:31:28 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -30,7 +30,7 @@ uuid_saved = ["fc81b935647d","260d27c38695","c5d83ec5fc2b","defaaeb70270","bbe4d
              "f36d794590f5","7d07ce8d8665","bcb99ccdd20c","9e36f86b0008","9ea42576fdaa","14dd987aade2","8e07e13880d9","a7d217159b3e","8651b2fea7b2","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","80f6025a1b65","0fe30d9aefb7","e73f19de755e","641de1a4ffef","a9dccb506c93","0c7b7b294bfd","fc71b4ce26ac","2230199d200d",
              "cdca8c0c222d","a34b330c24ae","c20af37ca609","0aa927848ddd","3fcce47d37b1","26b06274a393","c1db8b85552b","19235935724e","39d6d903b3e1","13c41aaca2e2",
-             "5a58382958f8"]
+             "5a58382958f8","57e691655447"]
 
 
 
@@ -156,7 +156,7 @@ dbfae3746772 = TheTwork
 fec12d3dfe = xtraRice
 45c31f67d6 = Bolt
 1f484708ec = Cekko
-8bdb6dba2f = RenjaHB
+57e691655447 = RenjaHB
 40dc55a18b = FamousFarrow
 2402f332f3 = Morticus
 4b49224938 = Sam Silva
