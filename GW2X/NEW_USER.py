672e289a6ec0a355d8b987a9675bfccb2983856d
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-11-22 13:43:04 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -26,7 +26,7 @@ thanks for your support !!!
 #=============
 
 uuid_saved = ["fc81b935647d","260d27c38695","c5d83ec5fc2b","defaaeb70270","bbe4dd66e5b7","defea4bf199a","7f2667bc6dfc","9012c84a0b40","50c039dd4579","7975cc14bdf2",
-             "0d1a1868620a","45e6a9b90206","28225c679d0e","dbfae3746772","4879801bc9b0","70350bfd0594","41a6b478d099","e47dd58e0bfe","21a5de4cdc05","3e5fe19c3927",
+             "0d1a1868620a","45e6a9b90206","28225c679d0e","dbfae3746772","4879801bc9b0","70350bfd0594","41a6b478d099","f0099246594c","21a5de4cdc05","3e5fe19c3927",
              "f36d794590f5","7d07ce8d8665","bcb99ccdd20c","9e36f86b0008","9ea42576fdaa","14dd987aade2","8e07e13880d9","a7d217159b3e","8651b2fea7b2","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","80f6025a1b65","0fe30d9aefb7","e73f19de755e","641de1a4ffef","a9dccb506c93","0c7b7b294bfd","fc71b4ce26ac","2230199d200d",
              "cdca8c0c222d","a34b330c24ae","c20af37ca609","0aa927848ddd","3fcce47d37b1","26b06274a393","c1db8b85552b","19235935724e","39d6d903b3e1","13c41aaca2e2",
@@ -201,7 +201,7 @@ b679c63915 = Luxz
 e53d258881 = Villain
 75a4159f86 = SquishyPoo1870
 21a5de4cdc05 = FRTZ
-e47dd58e0bfe = FRTZ 2
+f0099246594c = FRTZ 2
 01035bf5b7 = Akemi
 1680963910 = Robsie
 f106ab283d = J4XXXOR
