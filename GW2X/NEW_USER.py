339993030158037af37c18e4642edbbd962026ac
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2024-12-07 09:04:43 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -28,7 +28,7 @@ thanks for your support !!!
 uuid_saved = ["5f309f94c9e5","260d27c38695","c5d83ec5fc2b","defaaeb70270","bbe4dd66e5b7","defea4bf199a","7f2667bc6dfc","9012c84a0b40","50c039dd4579","7975cc14bdf2",
              "0d1a1868620a","bb1554a5ff91","28225c679d0e","dbfae3746772","4879801bc9b0","70350bfd0594","0f08565828a5","5711b4d1d871","465c5182bbf9","3e5fe19c3927",
              "f36d794590f5","7d07ce8d8665","bcb99ccdd20c","9e36f86b0008","bad596c1a665","14dd987aade2","8e07e13880d9","a7d217159b3e","8651b2fea7b2","9dfdb9eb4b2b",
-             "fffee4468210","da91f16e0273","80f6025a1b65","0fe30d9aefb7","e73f19de755e","641de1a4ffef","a9dccb506c93","0c7b7b294bfd","f0e939adcaa4","2230199d200d",
+             "fffee4468210","da91f16e0273","80f6025a1b65","0fe30d9aefb7","e73f19de755e","38fa8ee4da23","a9dccb506c93","0c7b7b294bfd","f0e939adcaa4","2230199d200d",
              "1dd34f147241","a34b330c24ae","c20af37ca609","0aa927848ddd","3fcce47d37b1","26b06274a393","c1db8b85552b","19235935724e","39d6d903b3e1","13c41aaca2e2",
              "5a58382958f8","57e691655447","e106470b7f05","fe43bbfa5c62","93df54d5a4f0","7f26bc6604d2","6ff42c310441","e004897274f4","bd184a1fed18","50e66cd69c85",
              "23dc807d0315","bd29391f1e21","735c23897fba","7d1490b1045c","b7b915e9d8ed"]
@@ -47,7 +47,7 @@ c1db8b85552b = robbyear
 0aa927848ddd = migetno1
 1dd34f147241 = Fallen
 2230199d200d = snoobie
-641de1a4ffef = serverleaderhunk Retired gamer
+38fa8ee4da23 = serverleaderhunk Retired gamer
 da91f16e0273 = hirobutton
 fffee4468210 = elpumita
 0f08565828a5 = Maerath f7ss27x5b4xbejoi
