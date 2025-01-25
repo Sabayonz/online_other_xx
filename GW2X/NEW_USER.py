ab3a905a93a101642a501d04eff9b19662439196
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-01-26 00:52:15 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -27,14 +27,14 @@ thanks for your support !!!
 
 uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4dd66e5b7","defea4bf199a","7f2667bc6dfc","cd8a7233c515","50c039dd4579","6f45e22ec708",
              "0d1a1868620a","bb1554a5ff91","28225c679d0e","dbfae3746772","4879801bc9b0","70350bfd0594","0f08565828a5","5711b4d1d871","465c5182bbf9","3e5fe19c3927",
-             "f36d794590f5","7d07ce8d8665","bcb99ccdd20c","98d32797f172","28bc9376b6ce","14dd987aade2","8e07e13880d9","a7d217159b3e","013360a945e8","9dfdb9eb4b2b",
+             "eb8bd8caffb6","7d07ce8d8665","bcb99ccdd20c","98d32797f172","28bc9376b6ce","14dd987aade2","8e07e13880d9","a7d217159b3e","013360a945e8","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","544cacde2182","0fe30d9aefb7","e73f19de755e","38fa8ee4da23","a9dccb506c93","0c7b7b294bfd","f0e939adcaa4","2230199d200d",
              "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","e85c424a4421","8cb166d967ba","e35062dbc245","19235935724e","39d6d903b3e1","13c41aaca2e2",
              "0f74bb9974c3","57e691655447","073ceb9bd760","fe43bbfa5c62","93df54d5a4f0","7f26bc6604d2","6ff42c310441","e004897274f4","bd184a1fed18","c4e9c8b26c0f",
              "23dc807d0315","cd4e04d53391","ec9c57476fba","4b08dcbfe5bb","2f80e32358b2","ef6ad16b03df","dca64f35a2a7","b53fe1c7db73","5f5d5a021b21","f26ad47ac5e6",
              "79ff43aa4fa8","0c96c8176cc5","3911dee33506","83742f4ed236","fb8bdeadf724","fc33b84d26a9","b683298fe41d","9e103acba888","818570036568","071c41b5d93b",
              "282208cc20f8","d5f8596da991","2eb699634770","aa4bdc5de6d1","b65512c57141","05783f867ba3","652c79d5e8c2","7d03588d2a2a","a1d2f539858a","f5bb35fc9656",
-             "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","eb8bd8caffb6"]
+             "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e"]
 
 
 
@@ -167,7 +167,7 @@ cd8a7233c515 = Don - oranjelly
 fe43bbfa5c62 = Shaxen bjokes
 ccda0f3c76 = Articvb
 1433e3fbf0 = Articvb 2
-f36d794590f5 = Private Marx
+eb8bd8caffb6 = unloadammo Private Marx
 7da945130a = Alcatraz 
 a9dccb506c93 = Kilmanagh
 c155f9c7d4 = dokyun
