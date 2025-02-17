859da348d6b2d6e8387bfe3ca645c07d700c96d3
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-02-17 16:24:56 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -34,7 +34,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4d
              "23dc807d0315","cd4e04d53391","390ced62abc5","4b08dcbfe5bb","2f80e32358b2","ef6ad16b03df","dca64f35a2a7","b53fe1c7db73","5f5d5a021b21","f26ad47ac5e6",
              "79ff43aa4fa8","0c96c8176cc5","58e1b54e500b","83742f4ed236","fb8bdeadf724","fc33b84d26a9","b683298fe41d","9e103acba888","818570036568","071c41b5d93b",
              "282208cc20f8","d5f8596da991","2eb699634770","aa4bdc5de6d1","b65512c57141","05783f867ba3","652c79d5e8c2","7d03588d2a2a","a1d2f539858a","c6a2035dd0a2",
-             "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","34fa18332624","97f6628d0558","d54e5e1398e4","2ee91806cdec"]
+             "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","34fa18332624","97f6628d0558","d54e5e1398e4","2ee91806cdec","47aa491dc988"]
 
 
 
@@ -234,7 +234,7 @@ f106ab283d = J4XXXOR
 7d07ce8d8665 = ivicask
 2eb699634770 = ivicask 2
 da698b0e4c = RarePepe
-d7e53d75f2 = Aiden Cross 
+47aa491dc988 = Aiden Cross 
 3496ec8c63 = tiredtbh
 e2823149be = Naksu
 01035bf5b7 = Beutelkramer
