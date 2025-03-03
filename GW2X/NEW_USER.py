7f78c246f0b79c6a9d4d3a081a61c5a92ac0b2fe
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-03-03 22:49:31 +0700

    Update NEW_USER.py

--- GW2X/NEW_USER.py
+++ GW2X/NEW_USER.py
@@ -27,7 +27,7 @@ thanks for your support !!!
 
 uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4dd66e5b7","defea4bf199a","7f2667bc6dfc","cd8a7233c515","50c039dd4579","6f45e22ec708",
              "88bf59952163","bb1554a5ff91","28225c679d0e","61725d1efcd0","4879801bc9b0","70350bfd0594","0f08565828a5","5711b4d1d871","465c5182bbf9","3e5fe19c3927",
-             "eb8bd8caffb6","7d07ce8d8665","bcb99ccdd20c","98d32797f172","eb2099e2bb81","14dd987aade2","8e07e13880d9","a7d217159b3e","013360a945e8","9dfdb9eb4b2b",
+             "eb8bd8caffb6","7d07ce8d8665","bcb99ccdd20c","98d32797f172","eb2099e2bb81","14dd987aade2","8e07e13880d9","a7d217159b3e","ce63e03d4633","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","544cacde2182","0fe30d9aefb7","e73f19de755e","38fa8ee4da23","a9dccb506c93","0c7b7b294bfd","73596a0d1b4b","407deca13712",
              "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","e85c424a4421","8cb166d967ba","e35062dbc245","19235935724e","39d6d903b3e1","13c41aaca2e2",
              "0f74bb9974c3","57e691655447","7fc15567bdc7","fe43bbfa5c62","93df54d5a4f0","7f26bc6604d2","6ff42c310441","e004897274f4","bd184a1fed18","c4e9c8b26c0f",
@@ -125,7 +125,7 @@ e85c424a4421 = Corvo (liarliarpunsonfyre)
 5fef652218 = Adam AC
 d28f134389 = temporary trial (removed)
 50c039dd4579 = rdenog
-013360a945e8 = MechFan ihateusingdisc yepdontcar
+ce63e03d4633 = MechFan ihateusingdisc yepdontcar
 0c28dc0eb4 = MMoaddict
 c0d43a2075 = Gumagan
 64e0c9c9d9 = Father Kofo
