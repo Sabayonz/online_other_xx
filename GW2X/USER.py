a40ea97ba56978154141d1cf005de6096c19b3fb
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-03-15 15:54:48 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710R|== \n 
+==|0002710M|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -33,7 +33,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4d
              "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","e85c424a4421","8cb166d967ba","e35062dbc245","19235935724e","39d6d903b3e1","13c41aaca2e2",
              "0f74bb9974c3","57e691655447","7fc15567bdc7","fe43bbfa5c62","93df54d5a4f0","7f26bc6604d2","6ff42c310441","e004897274f4","bd184a1fed18","c4e9c8b26c0f",
              "23dc807d0315","cd4e04d53391","390ced62abc5","4b08dcbfe5bb","2f80e32358b2","ef6ad16b03df","dca64f35a2a7","b53fe1c7db73","5f5d5a021b21","f26ad47ac5e6",
-             "79ff43aa4fa8","0c96c8176cc5","e6ede2333014","83742f4ed236","fb8bdeadf724","fc33b84d26a9","b683298fe41d","9e103acba888","818570036568","071c41b5d93b",
+             "79ff43aa4fa8","0c96c8176cc5","e6ede2333014","83742f4ed236","74495f9e6b24","fc33b84d26a9","b683298fe41d","9e103acba888","818570036568","071c41b5d93b",
              "282208cc20f8","d5f8596da991","2eb699634770","aa4bdc5de6d1","b65512c57141","05783f867ba3","652c79d5e8c2","575d468d903b","a1d2f539858a","c6a2035dd0a2",
              "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","34fa18332624","97f6628d0558","d54e5e1398e4","e659749a6440","47aa491dc988","b562d76f1419","2fb67367b2f3",
              "1da1095b530a","88184c0305ec","020ee934e806","3df932ca2794","d39882a5d11b","103b75ddae0d"]
@@ -269,7 +269,7 @@ c20af37ca609 = falco
 ecc010a66e = don't mind me 3
 b275bc6551 = don't mind me 2
 53b625f3d9 = don't mind me
-fb8bdeadf724 = morbiddarkblood
+74495f9e6b24 = morbiddarkblood
 cf80d1275b = NtajHmoob
 544cacde2182 = nemesis
 42ef9433ab99 = Nemesiss 2
