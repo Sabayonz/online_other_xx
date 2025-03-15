34d3a1439bbabacdacf9414ab8763dd174c866db
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-03-15 08:22:52 +0700

    Update CORE.py

--- GW2X/CORE.py
+++ GW2X/CORE.py
@@ -1,35 +1,37 @@
-xxxx1 = 0x0242A1A0
-xxxx2 = 0xC926D0
-xxxx3 = 0x14CC1BB
-xxxx4 = 0xC9CC66
-xxxx5 = 0x14CC1C4
-xxxx6 = 0x15353F9
-xxxx7 = 0x15353FD
-xxxx8 = 0x2429610
-xxxx9 = 0x277C9AC
+xxxx1 = 0x027E9FA0
+xxxx2 = 0xCCDAD0
+xxxx3 = 0x15238AB
+xxxx4 = 0xCD8396
+xxxx5 = 0x15238B4
+xxxx6 = 0x158D949
+xxxx7 = 0x158D94D
+xxxx8 = 0x2507360
+xxxx9 = 0x286070C
 xxxx10 = 30000
-xxxx11 = 0x0277E360
-xxxx12 = 0x02702D20
-xxxx13 = 0x28F9CC
-xxxx14 = 0x28F9C3
-xxxx15 = 0x6B0D8A
-xxxx16 = 0x6C3A81
-xxxx17 = 0x6C0A87
-xxxx18 = 0x6C2B85
-xxxx19 = 0x6C3DE8
-xxxx20 = 0x57C59F
-xxxx21 = 0x6C42B1
-xxxx22 = 0x6C44A4
-xxxx23 = 0x57BCED
+xxxx11 = 0x02862280
+xxxx12 = 0x027E6060
+xxxx13 = 0x29511C
+xxxx14 = 0x295113
+xxxx15 = 0x6C704A
+xxxx16 = 0x6DA011
+xxxx17 = 0x6D6FF7
+xxxx18 = 0x6D90B5
+xxxx19 = 0x14F83A6
+xxxx20 = 0x58F44F
+xxxx21 = 0x6DA837
+xxxx22 = 0x6DAA2A
+xxxx23 = 0x58EB8D
 xxxx24 = 0x0
+xxxx25 = 0x027E50C8
+
 
 #################
 zzzz1 = 8493291127782557243
 zzzz2 = 8493291127782555705
 zzzz3 = 2958583480223761423
 zzzz4 = 2958583480223761679
-zzzz5 = 3192451927280403699
-zzzz6 = 3192469519466448115
+zzzz5 = 4029769614250427635
+zzzz6 = 4029787206436472051
 zzzz7 = 1517730614438924275
 zzzz8 = 1517730614455701491
 zzzz9 = 1140
@@ -56,6 +58,14 @@ zzzz29 = 8417936958505357299
 zzzz30 = 8417936924145618931
 zzzz31 = 30581
 zzzz32 = 30580
+zzzz33 = 4083
+zzzz34 = 37008
+zzzz35 = 0xA6
+zzzz36 = 789519733
+zzzz37 = 789519732
+zzzz38 = 0x103
+zzzz39 = 997507205
+zzzz40 = 997572741
 
 
 
@@ -181,10 +191,8 @@ zzzz32 = 30580
 
 
 
-
-
-core_update = "Core: Oct_11_2024"
+core_update = "Core: Mar_15_2025"
 LWS4_EOD_AND_SOTO_MAP_LIST = [1226, 1248, 1526, 1517, 1509, 1510, 1371, 1015, 1317, 1428, 1422, 1490, 1438, 1442, 1452, 1550, 1554]
 pvp_pve_map = [350, 549, 1305, 1171, 554, 795, 1163, 900, 894, 875, 95, 96, 1099, 38, 899]
-versionnow = "1.0.0.3"
+versionnow = "1.0.0.1"
 messagex = "Err:The Game Just Got Updated!\nErr:Load Module Failed!\nErr:Task failed successfully.\nplease update the trainer"
