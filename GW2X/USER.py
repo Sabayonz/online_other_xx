073aebfb4879da95c98cb9e5d8fa341df519535e
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-03-19 00:31:19 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710V|== \n 
+==|0002710S|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -30,7 +30,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4d
              "88bf59952163","7bedc41ad5b6","28225c679d0e","61725d1efcd0","4879801bc9b0","70350bfd0594","93e18dc68b7a","5711b4d1d871","465c5182bbf9","6839045e57b0",
              "eb8bd8caffb6","7d07ce8d8665","bcb99ccdd20c","98d32797f172","eb2099e2bb81","14dd987aade2","8e07e13880d9","a7d217159b3e","ce63e03d4633","9dfdb9eb4b2b",
              "fffee4468210","da91f16e0273","544cacde2182","0fe30d9aefb7","e73f19de755e","38fa8ee4da23","a9dccb506c93","0c7b7b294bfd","73596a0d1b4b","407deca13712",
-             "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","e85c424a4421","8cb166d967ba","e35062dbc245","19235935724e","39d6d903b3e1","13c41aaca2e2",
+             "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","e85c424a4421","8cb166d967ba","e35062dbc245","19235935724e","b3f9d10fbe34","13c41aaca2e2",
              "0f74bb9974c3","57e691655447","7fc15567bdc7","fe43bbfa5c62","93df54d5a4f0","7f26bc6604d2","6ff42c310441","e004897274f4","bd184a1fed18","c4e9c8b26c0f",
              "23dc807d0315","cd4e04d53391","390ced62abc5","4b08dcbfe5bb","2f80e32358b2","ef6ad16b03df","dca64f35a2a7","b53fe1c7db73","5f5d5a021b21","f26ad47ac5e6",
              "79ff43aa4fa8","0c96c8176cc5","e6ede2333014","83742f4ed236","74495f9e6b24","fc33b84d26a9","b683298fe41d","7690f65354c2","818570036568","071c41b5d93b",
@@ -258,7 +258,7 @@ df33e92ff7 =  OD YR INK off
 73596a0d1b4b = Kaisen OD YR INK (hay_low)
 5eb7799781 = Syrix removed
 090afbe976 = Sinic
-39d6d903b3e1 = フォックスマン shermilion
+b3f9d10fbe34 = フォックスマン shermilion
 2ffeb1091c = polychromatik off
 c34b37dfd9 = polychromatik new
 5f5d5a021b21 = Reeeehaww
