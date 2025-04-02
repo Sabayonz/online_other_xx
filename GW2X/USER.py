6ec1eec2a3aea710f3e6db20d4d780f3c0854367
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-04-02 09:39:06 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710N|== \n 
+==|0002710R|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -37,7 +37,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4d
              "282208cc20f8","d5f8596da991","f9414e357b64","ebf1032d523f","b65512c57141","05783f867ba3","652c79d5e8c2","575d468d903b","a1d2f539858a","c6a2035dd0a2",
              "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","34fa18332624","97f6628d0558","d54e5e1398e4","e659749a6440","47aa491dc988","b562d76f1419","2fb67367b2f3",
              "1da1095b530a","88184c0305ec","020ee934e806","3df932ca2794","d39882a5d11b","103b75ddae0d","5011e1070865","157519e0ec0d","a1ae04cd9461","00d41dd5a3c6",
-             "67f0e3df0297","6e2877292a2c","253e0730a52e","59814b7991dd","d124ec0d35f4","f7eb05aa7621"]
+             "67f0e3df0297","6e2877292a2c","253e0730a52e","59814b7991dd","d124ec0d35f4","f7eb05aa7621","df112dffff14"]
 
 
 
@@ -93,7 +93,7 @@ d5d1d4a10d = hjjr78
 9fa1f56921b4 = clementming -
 bcb99ccdd20c = CraTzy
 9dfdb9eb4b2b = GNGR_VKNG
-63380e15ce = Runnae (saekoslab)
+df112dffff14 = Runnae (saekoslab)
 c62de1e69f = Kinjake
 666a11bf6e = Hizzy
 defaaeb70270 = Porpor
