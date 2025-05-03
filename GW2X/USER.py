3260eafdc8673c0cdef7902f48705ef66bdd7f5f
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-05-03 13:06:01 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -17,7 +17,7 @@ UL, RZ, AP, DW, GC, HM, SV, CG, CP, RW, VC, UM, PC, XR, NT, MC,
 KK, HF, MF, JN, PP, HZ, KJ, RN, GV, CT, CM, HJ, GK, DM, FL, MG, RB,
 BL, SN, DW, MM, GG, IC, RH, KW, SG, VR, AX, TX, MT, SW, RC, TK, AR,
 YK, BS, JD, CR, VL, RC, RO, SJ, HA, WT, LO, EV, SO, HC, QR, KJ, TR,
-FH, JD, FP \n
+FH, JD, FP, AJ \n
 ============================\n
 
 GW2X Exist and maintained since April 2023\nconsider donating to keep this project alive\nNO SUBSCRIBTION REQUIRED\n
@@ -40,12 +40,13 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","defaaeb70270","bbe4d
              "1da1095b530a","88184c0305ec","020ee934e806","3df932ca2794","d39882a5d11b","103b75ddae0d","5011e1070865","157519e0ec0d","01c394ddda5d","00d41dd5a3c6",
              "67f0e3df0297","6e2877292a2c","253e0730a52e","59814b7991dd","d124ec0d35f4","f7eb05aa7621","df112dffff14","7d3a76b1c4ce","676544bb325e","34d933a5ee67",
              "6436e69d0ed4","4739e0169158","680363e23cca","bbe2001e9659","08bb86dd2f0d","19f8d8d8a18f","bd8e021fc1b1","f28e5c15146a","9fd732c35ad3","a754914e029e",
-             "4de082e0a547","6225354ce65f","a967c2af7bb6","31cfe0807231"]
+             "4de082e0a547","6225354ce65f","a967c2af7bb6","31cfe0807231","de591bc31c94"]
 
 
 
 
 ''' 
+de591bc31c94 = ajlxxgb
 31cfe0807231 = felipemg1991
 a967c2af7bb6 = jose_dieguez
 a754914e029e = faulerhobbit_41913
