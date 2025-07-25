09b7f82992fbd4c1b4e7ecb2b9fd0b841828a249
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-07-25 07:17:42 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -42,7 +42,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4d
              "6436e69d0ed4","4739e0169158","680363e23cca","bbe2001e9659","08bb86dd2f0d","19f8d8d8a18f","bd8e021fc1b1","f28e5c15146a","9fd732c35ad3","a754914e029e",
              "4de082e0a547","6225354ce65f","a967c2af7bb6","31cfe0807231","de591bc31c94","8be26879a40d","9f5969b96917","c10884063354","49f02487967e","83da83063aad",
              "e989b877f979","854d2b1fe1e8","cc6c880179aa","86d0fe2b5751","1c115b3df591","e0620500eb2c","340df85b1842","9a4b4ccd9a86","86750e7fad6b","3f4533773c51",
-             "8dd2e0be4516"]
+             "8dd2e0be4516","19c6e8b88e7e"]
 
 
 
@@ -127,7 +127,7 @@ df112dffff14 = Runnae (saekoslab)
 83da83063aad = Porchy
 ae26cf86bc82 = universeelementor_63535
 a4b3afed94 = vector_4444 wise old man
-e99609f8e1 = reisw
+19c6e8b88e7e = reisw
 0fe30d9aefb7 = christmas_pud
 1ede003201 = chargovinda
 5011e1070865 = Silviohc
