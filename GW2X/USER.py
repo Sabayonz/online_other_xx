8f178939b8737d95eaba56843b909e21c4ae439c
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-08-23 09:45:39 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710Z|== \n 
+==|0002710B|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -45,7 +45,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4d
              "e989b877f979","854d2b1fe1e8","cc6c880179aa","86d0fe2b5751","1c115b3df591","e0620500eb2c","340df85b1842","9a4b4ccd9a86","86750e7fad6b","3f4533773c51",
              "8dd2e0be4516","19c6e8b88e7e","346a7c873716","7881c084bdf9","68ca0d53b9e1","6bc8e84aba67","963b49b26d3d","280e48064854","62bc6826ebaf","92f494b5a939",
              "de50d40e5e63","310bfa64c73b","8cadb5116a5f","f3da2ff2a3fa","5175c0d7d08c","f1df154f021f","c55bc3a6e78b","ac80cf35fa6e","c906460e1514","5bdf083e645e",
-             "14d2f4cedfa7"]
+             "14d2f4cedfa7","5a2ccabc5781"]
 
 
 
@@ -155,7 +155,7 @@ a34ede050a = morganj23
 e989b877f979 = gold3nb0i
 cd591db0ae = auburnsprings
 70350bfd0594 = david
-2730fe07c6 = beanboifresh
+5a2ccabc5781 = beanboifresh
 0f74bb9974c3 = tyrantt0_36151
 8b681b9078 = akulf123 riend
 e53d258881 = vekqurin
