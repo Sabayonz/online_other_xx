c4da5c683c8449a8afcee1e41cc9a5d53523647b
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-10-04 18:16:52 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710N|== \n 
+==|0002710G|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -34,7 +34,7 @@ uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4d
              "f7727cd2a815","da91f16e0273","544cacde2182","0fe30d9aefb7","e73f19de755e","38fa8ee4da23","a9dccb506c93","3f3e4bf4774e","73596a0d1b4b","407deca13712",
              "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","c82ceb50462f","8cb166d967ba","44ccb763a00e","19235935724e","b3f9d10fbe34","4e1f99da8f0e",
              "0f74bb9974c3","57e691655447","7fc15567bdc7","9d6ba27c6635","93df54d5a4f0","3d62670a8bed","6856f2dfdcc8","e004897274f4","58f6bf6ed1db","900325a0ab1f",
-             "454b4ed7edfc","d5dd3238f660","390ced62abc5","4b08dcbfe5bb","b14583af4be7","51507ffd6521","dca64f35a2a7","b53fe1c7db73","ca2787da7075","f26ad47ac5e6",
+             "454b4ed7edfc","d5dd3238f660","390ced62abc5","4b08dcbfe5bb","b14583af4be7","51507ffd6521","dca64f35a2a7","b53fe1c7db73","ca2787da7075","e0b9ceb8485f",
              "79ff43aa4fa8","d5935edeac66","ecc461b62d57","c58ae07ba84c","74495f9e6b24","fc33b84d26a9","b683298fe41d","02ffe4a90862","6a7c5f6cd0f6","a35329a359dc",
              "282208cc20f8","d5f8596da991","f9414e357b64","9942256edd23","b65512c57141","05783f867ba3","652c79d5e8c2","575d468d903b","a1d2f539858a","c6a2035dd0a2",
              "8b7ccd302fc3","7fd9ce29b273","15bb9bdd383e","ae26cf86bc82","97f6628d0558","14da12a731bc","7e1404419444","47aa491dc988","b562d76f1419","2fb67367b2f3",
@@ -151,7 +151,7 @@ a4b3afed94 = vector_4444 wise old man
 1ede003201 = chargovinda
 5011e1070865 = Silviohc
 a7d217159b3e = hamad771
-f26ad47ac5e6 = gr4cko
+e0b9ceb8485f = gr4cko
 f90bb82505 = nelson (doowen)
 515aee78c8 = appledinner
 f132a87d201f = razpdr
