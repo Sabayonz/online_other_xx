f470fc1a0e6c720a8b8d886ba1a5290f5ebbacf7
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2025-12-05 15:38:35 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -1,6 +1,6 @@
 
 donator = """
-==|0002710S|== \n 
+==|0002710f|== \n 
 LEGENDARY COMMANDER : \n
 -K W, -I P, -H W, D M M \n
 \n\n
@@ -29,7 +29,7 @@ thanks for your support !!!
 #=============
 
 uuid_saved = ["295fa0f2ba7f","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4dd66e5b7","defea4bf199a","7f2667bc6dfc","cd8a7233c515","28d007b687d5","6f45e22ec708",
-             "88bf59952163","7bedc41ad5b6","1303cfb17242","61725d1efcd0","4879801bc9b0","70350bfd0594","953c6fa8470a","5711b4d1d871","26776c09655e","6839045e57b0",
+             "88bf59952163","7bedc41ad5b6","1303cfb17242","61725d1efcd0","4879801bc9b0","70350bfd0594","410f7e68ce7c","5711b4d1d871","26776c09655e","6839045e57b0",
              "eb8bd8caffb6","e195c193612b","2a5e38a33dcd","be281bf3a788","eb2099e2bb81","14dd987aade2","8e07e13880d9","a7d217159b3e","ce63e03d4633","9dfdb9eb4b2b",
              "f7727cd2a815","da91f16e0273","544cacde2182","0fe30d9aefb7","2f407a8c87fc","38fa8ee4da23","a9dccb506c93","3f3e4bf4774e","73596a0d1b4b","407deca13712",
              "1dd34f147241","42ef9433ab99","c20af37ca609","0aa927848ddd","c82ceb50462f","8cb166d967ba","44ccb763a00e","19235935724e","cac006aac84f","4e1f99da8f0e",
@@ -135,7 +135,7 @@ e004897274f4 = iidwestii
 38fa8ee4da23 = serverleaderhunk Retired gamer
 da91f16e0273 = hirobutton
 f7727cd2a815 = elpumita
-953c6fa8470a = Maerath f7ss27x5b4xbejoi mig07zsrngsktn Fenlorn 
+410f7e68ce7c  = Maerath f7ss27x5b4xbejoi mig07zsrngsktn Fenlorn 
 c58ae07ba84c = davggmane
 c5d83ec5fc2b = gokiburi8296
 d5d1d4a10d = hjjr78
