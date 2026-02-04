ec5c069548bd261978cbf8b5d226c3da1469385c
Sabayonz <79715020+Sabayonz@users.noreply.github.com>
2026-02-05 04:51:31 +0700

    Update USER.py

--- GW2X/USER.py
+++ GW2X/USER.py
@@ -34,7 +34,7 @@ uuid_saved = ["e666a5b6610e","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4d
              "f7727cd2a815","da91f16e0273","544cacde2182","0fe30d9aefb7","2f407a8c87fc","38fa8ee4da23","a9dccb506c93","3f3e4bf4774e","73596a0d1b4b","407deca13712",
              "1dd34f147241","688b9ce0efc9","c20af37ca609","0aa927848ddd","c82ceb50462f","8cb166d967ba","44ccb763a00e","19235935724e","cac006aac84f","4e1f99da8f0e",
              "0f74bb9974c3","57e691655447","7fc15567bdc7","9d6ba27c6635","93df54d5a4f0","3d62670a8bed","6856f2dfdcc8","e004897274f4","58f6bf6ed1db","900325a0ab1f",
-             "cc7c18ac710c","43fa2d48b16d","390ced62abc5","4b08dcbfe5bb","b64ef812344a","51507ffd6521","dca64f35a2a7","b53fe1c7db73","ca2787da7075","e0b9ceb8485f",
+             "2a185766f3bf","43fa2d48b16d","390ced62abc5","4b08dcbfe5bb","b64ef812344a","51507ffd6521","dca64f35a2a7","b53fe1c7db73","ca2787da7075","e0b9ceb8485f",
              "79ff43aa4fa8","d5935edeac66","ecc461b62d57","c58ae07ba84c","74495f9e6b24","fc33b84d26a9","b683298fe41d","02ffe4a90862","f8a28c962ced","a35329a359dc",
              "282208cc20f8","d5f8596da991","f9414e357b64","5b370cab1e0a","b65512c57141","bc6d7b39acf8","415fe7093e11","575d468d903b","a1d2f539858a","d895b9f2c212",
              "8b7ccd302fc3","7fd9ce29b273","6c054fd2df08","b6cdedb0c3e5","97f6628d0558","8cb11ed67d1b","7e1404419444","47aa491dc988","b562d76f1419","2fb67367b2f3",
@@ -277,7 +277,7 @@ fc2c7ebc512a = Lucas--MileFantazy 4
 ca02c15aa2 = Taldor
 171ad776bc = akull123456
 1ff58644c8 = TheHacker old off
-cc7c18ac710c = TheHacker
+2a185766f3bf = TheHacker
 4e1f99da8f0e = Mizunie
 1bcd371a24 = Mizunie 2
 0d5192aaa8 = Mizunie 3
